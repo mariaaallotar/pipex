12ebcae1277baa57251dbbe38e9989a017c850ef
Maria Helenius <maheleni@c1r5p8.hive.fi>
2024-06-11 15:18:34 +0300

    Continued on argument checking functions and did piping function

--- pipex.c
+++ pipex.c
@@ -12,6 +12,7 @@
 
 #include "pipex.h"
 
+//TODO: check errors
 int	create_pipe(t_pipex	*s_pipex, char *infile, char *outfile)
 {
 	int	infile_fd;
@@ -22,39 +23,50 @@ int	create_pipe(t_pipex	*s_pipex, char *infile, char *outfile)
 		//TODO: handle error
 	}
 	infile_fd = open(infile, O_RDONLY);
+	if (infile_fd == -1)
+	{
+		//handle error
+	}
 	dup2(s_pipex->pipe_fds[0], infile_fd);
 	close(s_pipex->pipe_fds[0]);
-	close(infile_fd);	 //should this be closed?
-	//TODO: check that outfile exists, if not create one
-	outfile_fd = open(outfile, O_RDWR);
+	outfile_fd = open(outfile, O_WRONLY | O_CREAT, 0644);
+	if (outfile_fd == -1)
+	{
+		//handle error
+	}
 	dup2(s_pipex->pipe_fds[1], outfile_fd);
 	close(s_pipex->pipe_fds[1]);
-	close(outfile_fd);	//should this be closed?
 	return (1);
 }
 
 int	parse_command(char *command, t_pipex *s_pipex)
 {
-	//what happens if command is a path?
-
 	s_pipex->command_w_flags = mod_split(command, ' ', '\'');
 	if (s_pipex->command_w_flags == NULL)
 		return (-1);
+	if (access(s_pipex->command_w_flags[0], X_OK) == 0)
+		s_pipex->path = s_pipex->command_w_flags[0]
 	s_pipex->command = s_pipex->command_w_flags[0];
 	return (1);
 }
 
-int	parse_path(char *envp[], t_pipex *s_pipex)	//25 lines
+int	parse_path(char *envp[], t_pipex *s_pipex)
 {
 	char	**paths;
 	char	*command_path;
 	char	*path;
 	int		i;
 
-	//what happens if there is no PATH= variable in envp
+	if (s_pipe->path != NULL)
+		return (1);
 	i = 0;
-	while (ft_strnstr(envp[i], "PATH=", 5) == NULL)
+	while (envp[i] != NULL && ft_strnstr(envp[i], "PATH=", 5) == NULL)
 		i++;
+	if (envp[i] != NULL)
+	{
+		//TODO: handle error
+		return (-1);
+	}
 	paths = ft_split((const char *) (envp[i] + 5), ':');
 	i = 0;
 	while (paths[i] != NULL)
@@ -62,16 +74,58 @@ int	parse_path(char *envp[], t_pipex *s_pipex)	//25 lines
 		path = ft_strjoin(paths[i], "/");
 		command_path = ft_strjoin(path, s_pipex->command);
 		free(path);
-		if (access(command_path, F_OK) == 0)
+		if (access(command_path, X_OK) == 0)
 		{
 			s_pipex->path = command_path;
 			//free_everything(paths);		//needs to be implemented to free every string that this points to
-			return (1);
+			//handle error
+			return (-1);
 		}
 		free(command_path);
 		i++;
 	}
 	return (-1);
+}
+
+static void print_command(t_pipex *s_pipex)
+{
+	int i
+
+	i = 0;
+	printf("Command and all flags on seperate lines:\n\n");	//change to ft_printf
+	while (s_pipex.command_w_flags[i] != NULL)
+	{
+		printf("%s\n", s_pipex.command_w_flags[i]); //change to ft_printf
+		i++;
+	}
+	printf("\n"); //change to ft_printf
+}
+
+int	check_args(int argc, char *argv[], char *envp[], t_pipex *s_pipex)
+{
+	if (argc != 5)
+	{
+		printf("This progam needs exactly four arguments."); //change to ft_printf
+		return (0);
+	}
+	if (access(argv[1], R_OK) == -1)
+	{
+		//TODO: handle error
+		//no need to free anything
+	}
+	if (parse_command(argv[2], &s_pipex) == -1)
+	{
+		//TODO: handle error
+		//no need to free anything
+	}
+	print_command(&s_pipex); //only for debugging
+	if (parse_path(envp, &s_pipex) == -1)
+	{
+		//TODO: handle error
+	}
+
+	printf("Path to that command:\n");	//only for debugging
+	printf("%s\n", s_pipex.path);		//only for debugging
 }
 
 //TODO: remove this inclusion
@@ -80,35 +134,13 @@ int	parse_path(char *envp[], t_pipex *s_pipex)	//25 lines
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_pipex	s_pipex;
+	int		ret_value;
 
-	if (argc != 5)
+	ret_vlaue = check_args(argc, argv, envp, &s_pipex);
+	if ( ret_value == -1)
 	{
-		printf("This progam needs exactly four arguments."); //change to ft_printf
-		return (0);
+		//handle exit?
 	}
-	if (parse_command(argv[2], &s_pipex) == -1)
-	{
-		//TODO: handle error
-		//no need to free anything
-	}
-
-	// int i = 0;
-	// printf("Command and all flags on seperate lines:\n\n");
-	// while (s_pipex.command_w_flags[i] != NULL)
-	// {
-	// 	printf("%s\n", s_pipex.command_w_flags[i]);
-	// 	i++;
-	// }
-	// printf("\n");
-
-	if (parse_path(envp, &s_pipex))
-	{
-		//TODO: hadle error
-		//free 
-	}
-
-	printf("Path to that command:\n");
-	printf("%s\n", s_pipex.path);
-
 	create_pipe(&s_pipex, argv[1], argv[4]);
+	
 }
