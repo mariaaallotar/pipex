c8d2d52848c253562bba66d158c468c0adf15684
mariaaallotar <maria.helenius99@gmail.com>
2024-06-22 15:50:50 +0300

    Cntinued on the piping from different childprocesses, does not compile, not tested

--- pipex.c
+++ pipex.c
@@ -15,50 +15,71 @@
 //TODO: remove this inclusion
 #include <stdio.h>
 
-static void first_child_pipex(t_pipex s_pipex, int i)
+static void first_child_pipex(t_pipex *s_pipex, int i)
 {
-    dup2(STDIN_FILENO, s_pipex->argv[1]);
-    dup2(STDOUT_FILENO, s_pipex->fds[1]);
+	int infile;
+
+	infile = open(s_pipex->argv[1], O_RDONLY);
+    dup2(STDIN_FILENO, infile);
+    dup2(STDOUT_FILENO, s_pipex->pipe_fds[1]);
     // close();     //what to close?
-    execve(s_pipex->paths[i], (const char)s_pipex->cmds_w_flags[i], s_pipex->envp);
+    execve(s_pipex->paths[i], (const char *)s_pipex->cmds_w_flags[i], s_pipex->envp);
 }
 
-static void middle_child_pipex(t_pipex s_pipex, int i)
+static void middle_child_pipex(t_pipex *s_pipex, int i)
 {
-    dup2(STDIN_FILENO, s_pipex->fds[0]);
-    dup2(STDOUT_FILENO, fds[1]);
+    dup2(STDIN_FILENO, s_pipex->read_from_fd);
+    dup2(STDOUT_FILENO, s_pipex->pipe_fds[1]);
     // close();     //what to close?
-    execve(s_pipex->paths[i], (const char)s_pipex->cmds_w_flags[i], s_pipex->envp);
+    execve(s_pipex->paths[i], (const char *)s_pipex->cmds_w_flags[i], s_pipex->envp);
 }
 
-int	pipex(t_pipex s_pipex)
+static void last_child_pipex(t_pipex *s_pipex, int i)
+{
+	int	outfile;
+
+	outfile = open(s_pipex->argv[s_pipex->argc - 1], O_WRONLY);	//should also create that file if it does not exist
+    dup2(STDIN_FILENO, s_pipex->read_from_fd);
+    dup2(STDOUT_FILENO, outfile);
+    // close();     //what to close?
+    execve(s_pipex->paths[i], (const char *)s_pipex->cmds_w_flags[i], s_pipex->envp);
+}
+
+int	pipex(t_pipex *s_pipex)
 {
 	int		pid;
 	int		i;
+	int		argc;
 
-	if (pipe(s_pipex.pipe_fds) == -1)
-	{
-		//handle error: piping failed
-	}
+	argc = s_pipex->argc;
 	i = 0;
 	while (i < argc - 3)
 	{
-        s_pipex->read_from_fd = dup(s_pipex->fds[0]);
+		if (i != 0)
+			s_pipex->read_from_fd = dup(s_pipex->pipe_fds[0]);
+		if (i != argc - 3 - 1)
+		{
+			if (pipe(s_pipex->pipe_fds) == -1)
+			{
+				//handle error: piping failed
+			}
+		}
 		pid = fork();
 		if (pid == 0)
 		{
-			// handle_child_processes();    //what was I thinking here?
 			if (i == 0)
-				first_child_pipex(s_pipex, i)
+				first_child_pipex(s_pipex, i);
 			else if (i < argc - 3 - 1)
-				middle_child_pipex(s_pipex, i)
+			{
+				middle_child_pipex(s_pipex, i);
+			}
 			else
 				last_child_pipex(s_pipex, i);
 		}
 		i++;
 	}
-	close(s_pipex.pipe_fds[0]);
-	close(s_pipex.pipe_fds[1]);
-    free_struct;
+	close(s_pipex->pipe_fds[0]);
+	close(s_pipex->pipe_fds[1]);
+    //free_struct;
 	return (0);
 }
